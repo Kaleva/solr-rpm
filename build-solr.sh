36c01a51f4751aae3c61fd10b7f4ae4bb201f21c
Markus Syrjänen <markus.syrjanen@kaleva.fi>
2012-05-04 15:33:10 +0300

    Now using a mirror that's closer to us.

--- build-solr.sh
+++ build-solr.sh
@@ -5,7 +5,7 @@ mkdir -p BUILD RPMS SRPMS
 
 if [ ! -f SOURCES/apache-solr-$version.tgz ];
 then
-    wget "http://apache.cu.be/lucene/solr/$version/apache-solr-$version.tgz" -O SOURCES/apache-solr-$version.tgz
+    wget "http://ftp.wayne.edu/apache/lucene/solr/$version/apache-solr-$version.tgz" -O SOURCES/apache-solr-$version.tgz
 fi
 
 rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="ver $version" solr.spec
--- solr.spec
+++ solr.spec
@@ -6,7 +6,7 @@ Release:	2%{?dist}
 Summary:	Solr
 License:	GPL
 URL:		http://lucene.apache.org/solr/
-Source: 	http://apache.cu.be/lucene/solr/%{version}/apache-solr-%{version}.tgz
+Source: 	http://ftp.wayne.edu/apache/lucene/solr/%{version}/apache-solr-%{version}.tgz
 Source1:        tomcat-solr.xml
 Source2:        solrconfig.xml
 BuildRoot:	%(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
